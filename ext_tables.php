e435b8b5ec4b9975592e792f2fd6f1980bfe4aa5
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2018-02-14 12:08:48 +0000

    [TASK] Change extension icon

--- ext_tables.php
+++ ext_tables.php
@@ -18,7 +18,7 @@ if (TYPO3_MODE === 'BE') {
         ],
         [
             'access' => 'user,group',
-            'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+            'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.svg',
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_userlisting.xlf',
         ]
     );
