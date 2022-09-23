1baf6d8194b6d7956e1f43f58f19f99f476685f0
Fabien Udriot <fabien@omic.ch>
2022-09-23 15:55:41 +0200

    [TASK] Migrate backend user and user group to abstract entity

--- Classes/Domain/Repository/BackendUserGroupRepository.php
+++ Classes/Domain/Repository/BackendUserGroupRepository.php
@@ -2,26 +2,20 @@
 
 namespace Visol\Beusertools\Domain\Repository;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
-/**
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-/**
- * Repository for \TYPO3\CMS\Extbase\Domain\Model\BackendUserGroup.
- *
- * @api
- */
-class BackendUserGroupRepository extends \TYPO3\CMS\Extbase\Domain\Repository\BackendUserGroupRepository
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class BackendUserGroupRepository extends Repository
 {
+
+    public function initializeObject()
+    {
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
 
     protected $defaultOrderings = [
         'title' => QueryInterface::ORDER_ASCENDING
